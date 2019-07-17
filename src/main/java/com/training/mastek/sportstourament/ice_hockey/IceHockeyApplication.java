0623659ff1b134f96cb43d53271b38f4bb7df629
Elliot Corner <Elliot61163@Mastek.Com>
2019-07-17 10:32:31 +0100

    init

---
+++ src/main/java/com/training/mastek/sportstourament/ice_hockey/IceHockeyApplication.java
@@ -0,0 +1,13 @@
+package com.training.mastek.sportstourament.ice_hockey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceHockeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceHockeyApplication.class, args);
+	}
+
+}
